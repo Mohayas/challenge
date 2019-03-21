fd0a7bf35f908c9bfd5981ac8185ebfb9e743c39
El asas Mohamed <assass.mohamed@gmail.com>
2019-03-22 00:52:33 +0100

    merging streams, some work on initing the process

--- src/main/java/com/oraclelabs/techchallenge/core/PythonProcess.java
+++ src/main/java/com/oraclelabs/techchallenge/core/PythonProcess.java
@@ -16,6 +16,7 @@ public class PythonProcess {
 	private Process process;
 	private BufferedWriter writer;
 	private BufferedReader reader;
+	private BufferedReader errReader;
 
 	public PythonProcess() {
 		super();
@@ -23,31 +24,56 @@ public class PythonProcess {
 
 	private String getOutput() throws IOException {
 
+		String output = readOutput(reader);
+
+		// the subString just to remove ">>> " from the output,
+		// that's just because we're merging the sdtout with errout streams
+		return output.substring(4, output.length()).trim();
+	}
+
+	private String readOutput(BufferedReader reader) throws IOException {
+
 		String lines = "", line = "";
 		do {
 			line = reader.readLine();
-			if (!line.equals("[stop reading]"))
-				lines = lines + line + "\n";
+			if (!line.contains("[stop reading]"))
+				lines = lines + line + System.getProperty("line.separator");
 
-		} while (!line.equals("[stop reading]"));
+		} while (!line.contains("[stop reading]"));
 
 		return lines;
 	}
 
 	public void init() throws IOException, InterruptedException {
-		
+
+		//checking if the process is already initiated
 		if (this.process == null) {
-			
+
 			ProcessBuilder builder = new ProcessBuilder("python", "-iu");
 			
+			//piping the streams
 			builder.redirectInput(Redirect.PIPE);
 			builder.redirectOutput(Redirect.PIPE);
-			builder.redirectError(Redirect.PIPE);
+			builder.redirectErrorStream(true);
 			
+
 			this.process = builder.start();
-			
+
 			writer = new BufferedWriter(new OutputStreamWriter(process.getOutputStream()));
 			reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+			errReader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+
+			// importing sys because we need it to print errors
+			execute("from sys import stderr");
+
+			// printing [stop reading] to tell the reader to stop reading
+			execute("print(\"[stop reading]\", file=stderr)");
+
+			// we need to read before running the user code, so we clear the
+			// output
+			String str = readOutput(reader);
+			System.out.println("first output" + str);
+
 		}
 	}
 
@@ -56,9 +82,13 @@ public class PythonProcess {
 	}
 
 	public String runCode(String code) throws IOException, InterruptedException {
-
+		
+		//initiating the process
 		init();
 		execute(code);
+
+		// just so we can tell the reader to stop reading.
+		execute("print (\"[stop reading]\")");
 		return getOutput();
 
 	}
@@ -66,9 +96,6 @@ public class PythonProcess {
 	private void execute(String code) throws IOException {
 
 		writer.write(code);
-		writer.newLine();
-		writer.flush();
-		writer.write("print (\"[stop reading]\")");
 		writer.newLine();
 		writer.flush();
 
